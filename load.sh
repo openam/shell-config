1fd75ef552b698f4f06c480183b830526efe0ede
Justin Morris <morris.justin@gmail.com>
2015-04-21 12:29:32 -0600

    fix: only load scripts for current operating system

--- load.sh
+++ load.sh
@@ -8,7 +8,22 @@ for config in $HOME/.shell-config/config/*; do
 	source $config;
 done
 
+# Add ./bin folder to PATH and set as executable
+export PATH=$HOME/.shell-config/bin:$PATH
 for file in $HOME/.shell-config/bin/*; do
+	chmod +x $file
+done
+
+# For OS X binaries only. Add ./bin folder to PATH and set as executable
+if [[ $(uname -s) == "Darwin" ]]; then
+	export PATH=$HOME/.shell-config/bin/osx:$PATH
+
+	for file in $HOME/.shell-config/bin/osx/*; do
+		chmod +x $file
+	done
+fi
+
+for file in $HOME/.shell-config/setup/*; do
 	chmod +x $file
 done
 
--- zshrc
+++ zshrc
@@ -53,7 +53,7 @@ plugins=(git docker)
 
 # User configuration
 
-export PATH=$HOME/bin:$HOME/.shell-config/bin:/usr/local/bin:$PATH
+export PATH=$HOME/bin:/usr/local/bin:$PATH
 # export MANPATH="/usr/local/man:$MANPATH"
 
 source $ZSH/oh-my-zsh.sh
