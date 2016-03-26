716595bea5942a49e20231980531396060003c73
Michael Tuttle <openam@gmail.com>
2016-03-26 17:01:39 -0600

    fix: check to make sure directory does NOT exist prior to installing

--- setup/zsh.sh
+++ setup/zsh.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ -d .oh-my-zsh ]; then
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
 	echo "Installing oh-my-zsh"
 
 	curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
