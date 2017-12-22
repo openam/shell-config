2c7f92cf799c9b2953fddb68c9ba7ce79e31c3a0
Michael Tuttle <openam@gmail.com>
2017-12-22 16:27:13 -0700

    refactor: update zsh setup with custom functions

--- setup/zsh.sh
+++ setup/zsh.sh
@@ -12,26 +12,33 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
 	curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 fi
 
-DIRECTORY="$CUSTOM_PLUGINS/zsh-nvm"
-if [ ! -d "$DIRECTORY" ]; then
-	echo "Installing zsh-nvm"
+function update_gitrepo() {
+	local name=$(basename $1)
+	read -p "Would you like to update $name (Y/n)? " -r
+	if [[ $REPLY =~ ^[Yy]([Ee][Ss])?$ ]] || [[ -z $REPLY ]]; then
+		echo "Updating $1..."
+		git -C $1 pull
+		echo
+	fi
+}
 
-	git clone https://github.com/lukechilds/zsh-nvm $DIRECTORY
-fi
+function install_or_update() {
+	local name=$(basename $2)
+	local DIRECTORY="$CUSTOM/$1/$name"
 
-DIRECTORY="$CUSTOM_PLUGINS/zsh-syntax-highlighting"
-if [ ! -d "$DIRECTORY" ]; then
-	echo "Installing zsh-syntax-highlighting"
+	if [ ! -d "$DIRECTORY" ]; then
+		echo "Installing $name"
 
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$DIRECTORY"
-fi
+		git clone $2 $DIRECTORY
+		echo
+	else
+		update_gitrepo $DIRECTORY
+	fi
+}
 
-DIRECTORY="$CUSTOM_THEMES/powerlevel9k"
-if [ ! -d "$DIRECTORY" ]; then
-	echo "Installing powerlevel9k"
-
-	git clone https://github.com/bhilburn/powerlevel9k.git "$DIRECTORY"
-fi
+install_or_update plugins https://github.com/lukechilds/zsh-nvm
+install_or_update plugins https://github.com/zsh-users/zsh-syntax-highlighting
+install_or_update themes https://github.com/bhilburn/powerlevel9k
 
 zshrc="$HOME/.shell-config/zshrc"
 target="$HOME/.zshrc"
